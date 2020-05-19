3c62c48fab2874658ba8d9dbbbf9d94c552a17c0
DAQAC <64597147+DAQAC@users.noreply.github.com>
2020-05-19 03:34:26 +0100

    JDBC DRIVER
    The JDBC driver to connect the MySQL database with Java.

---
+++ JDBCDriver.java
@@ -0,0 +1,11 @@
+package main;
+
+public class JDBCDriver {
+	public static final String DB_URL = "jdbc:mysql://localhost/inventory_project?serverTimezone=UTC";
+	public static final String USER = "root";
+	public static final String PASS = "root";
+
+	private JDBCDriver() {
+	}
+
+}
