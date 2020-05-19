130c8677b40d7f3938c6c64344fee2041d9b9601
DAQAC <64597147+DAQAC@users.noreply.github.com>
2020-05-19 03:35:45 +0100

    Inventory Project Runner
    The latest version of the main Inventory Project Runner Application; which allows you to access all 4 tables and their functionalities.

---
+++ Runner.java
@@ -0,0 +1,72 @@
+package main;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Runner {
+
+//	public static void logIn() {
+//		Inventory_Login userlog = new Inventory_Login();
+//		userlog.InventoryLog();
+//	}
+
+	private static Scanner scan = new Scanner(System.in);
+
+	private static String getAction() {
+		System.out.println("______Daryl's Manga Shop______ \n" + "-----------------------------------");
+		System.out.println("Type 'customer' for CUSTOMER Table: \n" + "Type 'products' for PRODUCTS Table: \n"
+				+ "Type 'orders' for ORDERS Table: \n" + "Type 'categories' for CATEGORIES Table: \n"
+				+ "Type 'user' for USER Table:");
+		return scan.nextLine().toLowerCase();
+	}
+
+	public static void main(String[] args) throws SQLException {
+
+		try {
+			String action = "";
+			action = getAction();
+
+			do {
+
+				switch (action) {
+
+				case "customer":
+					CustomerApp cus = new CustomerApp();
+					cus.CustomerRunner();
+					break;
+
+				case "products":
+					ProductsApp pro = new ProductsApp();
+					pro.ProductsRunner();
+					break;
+
+				case "orders":
+					OrdersApp ord = new OrdersApp();
+					ord.OrdersRunner();
+					break;
+
+				case "categories":
+					CategoriesApp cat = new CategoriesApp();
+					cat.CategoriesRunner();
+					break;
+
+				case "user":
+					UserApp use = new UserApp();
+					use.UserRunner();
+					break;
+				}
+
+				action = getAction();
+
+			} while (!action.equals("quit"));
+			System.out.println("Program has Ended");
+
+		} finally {
+
+			scan.close();
+
+		}
+
+	}
+
+}
